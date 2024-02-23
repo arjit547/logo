e7aef29785a32463c8feb892cba0e9c69b01bd40
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-02-23 14:14:26 +0530

    Update integrations-deploy-post.sh

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
 
+# Check if the deployment group is 'songit'
 if [ "$DEPLOYMENT_GROUP_NAME" == "songit" ]; then
-    if [ -e /home/my-temp-dir/.env ]; then
-        echo "Waiting for 2 minutes...."
-        sleep 120
-        cp -R /home/my-temp-dir/. /var/www/html
-        rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
-        npm install
-        #npm install -g pkg
-
-        # Run npm build and check its exit code
-        #npm run build
-
-        if [ $? -ne 0 ]; then
-            echo "Error: npm run build failed."
-        fi
-    else
-        cp -R /home/my-temp-dir/. /var/www/html
-        rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
-        npm install
-
-        # Run npm build and check its exit code
-        #npm run build
-
-        if [ $? -ne 0 ]; then
-            echo "Error: npm run build failed."
-        fi
-    fi
-
-    # Installing Git and Git secrets
+    # Install Git and Git secrets
     echo "Installing Git"
     apt-get update
     apt-get install git -y
@@ -42,9 +12,38 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "songit" ]; then
     tar -xzf 1.3.0.tar.gz
     cd git-secrets-1.3.0 && sudo make install && cd ..
     git-secrets --register-aws
+
+    # Scan the repository for secrets
+    echo "Scanning repository for secrets"
     git-secrets --scan -r .
+
+    # Check Git secrets scan exit code
     if git-secrets --scan -r . | grep -q 'ERROR'; then
+        echo "Error: Secrets found in the repository. Aborting deployment."
         exit 1
+    else
+        echo "No secrets found in the repository. Proceeding with deployment."
     fi
 
+    # Copy files to destination
+    if [ -e /home/my-temp-dir/.env ]; then
+        echo "Waiting for 2 minutes...."
+        sleep 120
+    fi
+
+    echo "Copying files to destination..."
+    cp -R /home/my-temp-dir/. /var/www/html
+    rm -rf /home/my-temp-dir
+    chown -R ubuntu:ubuntu /var/www/html
+    cd /var/www/html
+
+    # Install npm dependencies
+    echo "Installing npm dependencies..."
+    npm install
+
+    # Check npm install exit code
+    if [ $? -ne 0 ]; then
+        echo "Error: npm install failed."
+        exit 1
+    fi
 fi
